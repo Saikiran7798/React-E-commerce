7f2a5daee21366c0676f767ca25a92e0c2d4dd1d
Saikiran Reddy <saikiran.af536@gmail.com>
2024-02-20 23:30:25 -0500

    Added Signup Page

--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import './CSS/LoginSingup.css'
 
 const LoginSignup = () => {
   return (
-    <div>LoginSignup</div>
+    <div className='loginsignup'>
+      <div className="loginsignup-container">
+        <h2>Sign Up</h2>
+        <div className="loginsignup-fields">
+          <input type='text' placeholder='Your name' />
+          <input type="email" placeholder='Email Address'/>
+          <input type="password" placeholder='Password' />
+        </div>
+        <button>Continue</button>
+        <p className="loginsignup-login">
+          Already have an account? <span>Login here</span>
+        </p>
+        <div className="loginsignup-agree">
+          <input type="checkbox" name="" id="" />
+          <p>By Continuing I agree to the terms of use & privacy Policy</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
