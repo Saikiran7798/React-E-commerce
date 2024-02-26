346f80f42be61df301733523adbe00c4d3e7350c
Saikiran Reddy <saikiran.af536@gmail.com>
2024-02-26 14:59:50 -0500

    Added media queries for navbar

--- frontend/src/Components/Navbar/Navbar.js
+++ frontend/src/Components/Navbar/Navbar.js
@@ -1,21 +1,29 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useRef, useState } from 'react'
 import './Navbar.css'
 import logo from '../Assets/logo.png'
 import cart_icon from '../Assets/cart_icon.png'
 import { Link } from 'react-router-dom'
 import { ShopContext } from '../../Context/ShopContext'
+import dropdown_icon from '../Assets/navbar_dropdown.png'
 
 const Navbar = () => {
 
   const [menu, setMenu] = useState("shop")
   const {cartTotal} = useContext(ShopContext)
+  const menuRef = useRef(null)
+
+  const dropdown_toggle = (event) => {
+    menuRef.current.classList.toggle('nav-menu-visible')
+    event.target.classList.toggle('open')
+  }
   return (
     <div className='navbar'>
       <div className="nav-logo">
         <img src={logo} alt="" />
         <p>Shopper</p>
       </div>
-      <ul className="nav-menu">
+      <img src={dropdown_icon} alt="" onClick={dropdown_toggle} className='nav-dropdown' />
+      <ul ref={menuRef} className="nav-menu">
         <li onClick={() => setMenu("shop")}><Link to ='/'>Shop</Link>{menu === "shop" ? <hr /> : null}</li>
         <li onClick={() => setMenu("mens")}><Link to ='/mens'>Men</Link> {menu === "mens" ? <hr /> : null}</li>
         <li onClick={() => setMenu("womens")}><Link to ='/womens'>Women</Link> {menu === "womens" ? <hr /> : null}</li>
