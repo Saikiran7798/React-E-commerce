9e358baaf2fe753a8fdd4847098308608678fada
Saikiran Reddy <saikiran.af536@gmail.com>
2024-03-21 23:35:36 -0400

    Added SortBy to Products

--- frontend/src/Pages/ShopCategory.jsx
+++ frontend/src/Pages/ShopCategory.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useMemo, useState } from 'react'
 import './CSS/ShopCategory.css'
 import { ShopContext } from '../Context/ShopContext'
 import dropdown_icon from '../Components/Assets/dropdown_icon.png'
@@ -6,34 +6,64 @@ import Item from '../Components/Item/Item'
 
 
 const ShopCategory = (props) => {
-  const {all_products} = useContext(ShopContext)
+  const { all_products } = useContext(ShopContext)
+  const [showNumber, setShowNumber] = useState(12)
+  const cat_products = useMemo(() => {
+    return all_products.filter((item) => item.category === props.category)
+  }, [all_products, props.category])
+  const [showProducts, setShowProducts] = useState([])
+  const [showSort, setShowSort] = useState(false)
+
+  const handleExploreClick = (event) => {
+    event.preventDefault();
+    if (showNumber + 12 > cat_products.length) {
+      setShowNumber(cat_products.length)
+    }
+    else {
+      setShowNumber(showNumber + 12)
+    }
+
+    console.log("Hello")
+  }
+
+  const handleSort = () => {
+    setShowSort(!showSort)
+  }
+  const handleLTH = () => {
+    showProducts.sort((a,b) => (a.new_price - b.new_price))
+  }
+  const handleHTL = () => {
+    showProducts.sort((a,b) => (b.new_price - a.new_price))
+  }
+  useEffect(() => {
+    setShowProducts(cat_products.slice(0, showNumber))
+  }, [showNumber, cat_products])
   return (
     <div className='shopcategory'>
       <img className='shopcategory-banner' src={props.banner} alt='' />
       <div className="shopcategory-index-sort">
         <p>
-          <span>Showing 1-12</span> out of 36 matches
+          <span>Showing 1-{showNumber}</span> out of {cat_products.length} matches
         </p>
-        <div className="shopcategory-sort">
-          Sort by <img src={dropdown_icon} alt=''/>
+        <div onClick={handleSort} className="shopcategory-sort">
+          Sort by <img src={dropdown_icon} alt='' />
+          {showSort ? (<div className="sort">
+            <button onClick={handleLTH}>Price: Low to High</button>
+            <button onClick={handleHTL}>Price: High to Low</button>
+          </div>) : null}
         </div>
       </div>
       <div className="shopcategory-products">
-        {all_products.map((item, index) => {
-          if(props.category === item.category) {
-            return(
-              <Item key={index} id={item.id} name={item.name} image={item.image} new_price={item.new_price}
-            old_price={item.old_price} />
-            )
-          }
-          else {
-            return null
-          }
+        {showProducts.map((item, index) => {
+          return (
+            <Item key={item.id} id={item.id} name={item.name} image={item.image} new_price={item.new_price}
+              old_price={item.old_price} />
+          )
         })}
       </div>
-      <div className="shopcategory-loadmore">
+      <button onClick={handleExploreClick} className="shopcategory-loadmore">
         Explore More
-      </div>
+      </button>
     </div>
   )
 }
